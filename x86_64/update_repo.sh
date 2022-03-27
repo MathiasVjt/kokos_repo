dde446a196e23699b8979a46960e1daa91ebe9f6
MathiasVjt <vojtek.mathias@icloud.com>
2022-03-27 17:14:32 +0200

    update

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-rm alci_repo*
+rm kokos_repo*
 
 echo "repo-add"
-#repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-repo-add -n -R alci_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R kokos_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
-#cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
 echo "####################################"
 echo "Repo Updated!!"
